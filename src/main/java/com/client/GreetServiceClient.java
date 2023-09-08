ac8812b0503bd68730feaff211c9dc069db3edcc
Ganesh Dhore <ganesh.dhore@austere.co.in>
2023-09-08 12:28:08 +0530

    first commit

---
+++ src/main/java/com/client/GreetServiceClient.java
@@ -0,0 +1,11 @@
+package com.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name="GREET-SERVICE")
+public interface GreetServiceClient {
+
+	@GetMapping("/greet")
+	public String invokeGreetService();
+}
---
+++ src/main/java/com/controller/Welcome.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.client.GreetServiceClient;
+
+@RestController
+public class Welcome {
+	
+	@Autowired
+	private GreetServiceClient greetClient;
+	
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/welcome")
+	public String get() {
+		String port = env.getProperty("server.port");
+		
+		String invokeGreetService = greetClient.invokeGreetService();
+		
+		return "Welcmoe to Service-1 ( WELCOME ) "+invokeGreetService+":: and port number ================="+port;
+	}
+	
+}
